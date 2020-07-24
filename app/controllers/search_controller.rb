245609458d17db15f69499e7ab8ff5a0b9ce5fbe
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-07-24 10:28:55 -0600

    add nearest_station to search_controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,9 @@
 class SearchController < ApplicationController
   def index
+    station_service = StationService.new
+    nearest_station = station_service.nearest_station(params[:location], 1)
+    station = Station.new(nearest_station)
+    map_service = MapService.new
+    directions = map_service.get_directions(params[:location], station.address)
   end
 end
