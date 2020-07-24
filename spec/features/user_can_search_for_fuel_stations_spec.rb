9e069252b5dc05b4080ff8d457045524c3a3c769
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-07-24 09:31:52 -0600

    add station data and directions to test

--- spec/features/user_can_search_for_fuel_stations_spec.rb
+++ spec/features/user_can_search_for_fuel_stations_spec.rb
@@ -7,5 +7,12 @@ describe "user can search for fuel stations" do
     click_button 'Find Nearest Station'
 
     expect(current_path).to eq(search_path)
+    expect(page).to have_content('Station Name')
+    expect(page).to have_content('Station address')
+    expect(page).to have_content('Station fuel types')
+    expect(page).to have_content('Station access times')
+    expect(page).to have_content('Distance to station: 0.1 miles')
+    expect(page).to have_content('Estimated travel time: 1 minute')
+    expect(page).to have_content("Turn left onto Lawrence St Destination will be on the left")
   end
 end
